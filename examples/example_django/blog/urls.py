03e0ea6d6388648aaf550cb180bf6d19feef158b
Hugo Herter <git@hugoherter.com>
2021-07-07 09:46:16 +0200

    WIP: Save state in Django example

--- examples/example_django/blog/urls.py
+++ examples/example_django/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import ArticleListView, CommentFormView, test_view
+from .views import ArticleListView, CommentFormView, save_state
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article-list"),
-    path("comment", csrf_exempt(CommentFormView.as_view()), name="comment"),
-    path("post", csrf_exempt(test_view), name="test-post"),
+    path("comment", CommentFormView.as_view(), name="comment"),
+    path("state/save", save_state, name="save-state"),
 ]
--- examples/example_django/blog/views.py
+++ examples/example_django/blog/views.py
@@ -1,7 +1,11 @@
 import os
+from datetime import datetime
 
+from aleph_client.asynchronous import create_store
+from aleph_client.chains.remote import RemoteAccount
+from django.conf import settings
 from django.http import JsonResponse
-from django.views.generic import ListView, FormView, CreateView
+from django.views.generic import ListView, CreateView
 
 from .forms import CommentForm
 from .models import Article
@@ -20,6 +24,20 @@ class CommentFormView(CreateView):
     success_url = "/"
 
 
-def test_view(request):
-    print(request.POST)
-    return JsonResponse(request.POST)
+async def save_state(request):
+    db_path: str = settings.DATABASES['default']['NAME']
+    with open(db_path, 'rb') as db_file:
+        content: bytes = db_file.read()
+
+    account = await RemoteAccount.from_crypto_host(
+        host="http://localhost", unix_socket="/tmp/socat-socket")
+
+    response = await create_store(
+        account=account,
+        file_content=content,
+        ref=None,
+        channel="TEST",
+        storage_engine="storage",
+    )
+    print(response)
+    return JsonResponse({'hello': 'world'})
