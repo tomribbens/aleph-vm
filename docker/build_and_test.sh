e63d22b324da534a55dda148bcf39d3320c4f513
Hugo Herter <git@hugoherter.com>
2021-12-13 13:26:15 +0100

    WIP: Docker tests

--- docker/build_and_test.sh
+++ docker/build_and_test.sh
@@ -8,22 +8,22 @@ else
   DOCKER_COMMAND=podman
 fi
 
-$DOCKER_COMMAND build -ti -t aleph-vm-supervisor -f docker/vm_supervisor.dockerfile .
+$DOCKER_COMMAND build -ti -t aleph-vm-supervisor-dev -f docker/vm_supervisor-dev.dockerfile .
 
 $DOCKER_COMMAND run -ti --rm \
-  -v $(pwd):/root/aleph-vm \
+  -v $(pwd):/opt/aleph-vm \
   --device /dev/kvm \
   -v /dev/kvm:/dev/kvm \
   --privileged \
   --name vm_supervisor \
-  aleph-vm-supervisor \
+  aleph-vm-supervisor-dev \
   python3 -m vm_supervisor -p -vv --system-logs --benchmark 1 --profile
 
 $DOCKER_COMMAND run -ti --rm \
-  -v $(pwd):/root/aleph-vm \
+  -v $(pwd):/opt/aleph-vm \
   --device /dev/kvm \
   -v /dev/kvm:/dev/kvm \
   --privileged \
   --name vm_supervisor \
-  aleph-vm-supervisor \
+  aleph-vm-supervisor-dev \
   pytest -vv -x vm_supervisor
